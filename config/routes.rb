6a26a867b0044cbb15476e8cbc6598b32485293c
Patrick McKernin <patrick@firstdraft.com>
2020-05-19 09:42:33 -0500

    changed routes file and controller to remove the shortcut for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   # =============================
 
-  root("vision#homepage")
+  #root("vision#homepage")
+  get("/",  { :controller => "vision", :action => "homepage" })
 
   get("/vision/:detector", { :controller => "vision", :action => "form" })
   post("/vision/:detector", { :controller => "vision", :action => "magic" })
